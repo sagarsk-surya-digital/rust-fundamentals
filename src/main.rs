83084eb1fd371225c8783fc011e6de78369fe202
sagarsk-surya-digital <sagar.sk@surya-digital.com>
2022-12-03 11:32:32 +0530

    something

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+pub mod weight_calculator;
+
 fn main() {
-    println!("Hello, world!");
-}
+    weight_calculator::function()
+} 
+
+//basic data type
+/*
+    u8, i8 -> unsigned and signed integer (8 indicates 8 bits)
+    f32, f64 -> float
+    bool true and false
+    char 
+
+*/
